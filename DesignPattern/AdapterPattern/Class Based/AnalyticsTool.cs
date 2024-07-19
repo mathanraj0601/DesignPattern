358816ffe4b18c672591f1fdbbe586b0a120efdb
Mathanraj <madhanmani0601@gmail.com>
2024-07-19 09:59:08 +0530

    Added Adapter

---
+++ DesignPattern/AdapterPattern/Class Based/AnalyticsTool.cs
@@ -0,0 +1,14 @@
+﻿namespace AdapterPattern.Class_Based
+{
+    // Adaptee
+    public class AnalyticsTool
+    {
+        public void AnalyzeData(String JsonData)
+        {
+            if (JsonData.EndsWith("json"))
+                Console.WriteLine("Analyzing Json Data");
+            else 
+                Console.WriteLine("Incompatible Data ");
+        }
+    }
+}
---
+++ DesignPattern/AdapterPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using AdapterPattern.Object_Based;
+using AdapterPattern.Class_Based;
+
+
+// Object Based
+//IAdapter xMLtoJSONConverter = new XMLtoJSONConverter("data");
+//xMLtoJSONConverter.Request();
+
+// Class Based
+
+IAdapter adapter = new XMLtoJSONConverter("data");
+adapter.Request();
